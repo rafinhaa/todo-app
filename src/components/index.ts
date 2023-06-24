cc7bd76a975af7cc1a10cc56fa2404a2c2d3fbf1
Rafinhaa <rafinhaa@me.com>
2023-06-24 16:42:31 -0300

    feat: Export componentes

---
+++ src/components/index.ts
@@ -0,0 +1,9 @@
+import AddTaskButton from "./Buttons/AddTaskButton";
+import Input from "./Input";
+import Header from "./Header";
+import ListTasksEmpty from "./ListTasksEmpty";
+import Space from "./Space";
+import Task from "./Task";
+import TasksInfo from "./TasksInfo";
+
+export { AddTaskButton, Header, Input, ListTasksEmpty, Space, Task, TasksInfo };
