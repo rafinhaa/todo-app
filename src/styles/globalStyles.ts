397ed138701f0dde438e008cb4623c69588a0710
Rafinhaa <rafinhaa@me.com>
2023-06-24 12:18:41 -0300

    feat: Estilos globais

---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,32 @@
+const fontSizes = {
+  small: 12,
+  medium: 14,
+  large: 16,
+};
+
+export const globalStyles = {
+  colors: {
+    blueDark: "#1e6f9f",
+    blue: "#4ea8de",
+    purpleDark: "#5e60ce",
+    purple: "#8284fa",
+    gray700: "#0d0d0d",
+    gray600: "#1a1a1a",
+    gray500: "#262626",
+    gray400: "#333333",
+    gray300: "#808080",
+    gray200: "#d9d9d9",
+    gray100: "#f2f2f2",
+    danger: "#e25858",
+  },
+  fonts: {
+    regular: "Inter_400Regular",
+    bold: "Inter_700Bold",
+  },
+  lineHeights: {
+    small: fontSizes.small * 1.4,
+    medium: fontSizes.medium * 1.4,
+    large: fontSizes.large * 1.4,
+  },
+  fontSizes,
+};
