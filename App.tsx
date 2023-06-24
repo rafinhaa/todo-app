9f14265ebe6cc87c30fc0dc92ea45bc2c97c0187
Rafinhaa <rafinhaa@me.com>
2023-06-24 16:45:23 -0300

    feat: Implementa screen Home

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import {
   useFonts,
   Inter_400Regular,
   Inter_700Bold,
 } from "@expo-google-fonts/inter";
+
+import { Home } from "./src/screens";
+import { StatusBar, View } from "react-native";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -17,24 +18,13 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text
-        style={{
-          fontFamily: "Inter_700Bold",
-        }}
-      >
-        Open up App.tsx to start working on your app!
-      </Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <StatusBar
+        barStyle={"light-content"}
+        backgroundColor={"transparent"}
+        translucent
+      />
+      <Home />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
